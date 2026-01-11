4e5d1e814a18b9f96ddea913ad551ac3cc1a833a
Telechi Nicolae <tux2nicolae@gmail.com>
2026-01-11 16:10:54 +0200

    Define production

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
       insertTypesEntry: true,
     }),
   ],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
